96004cab6f1506b6ebcffaa4c609b2ee229ef74d
jasdsj <jasds2002@gmail.com>
2024-03-27 11:52:24 +0100

    yos

--- lab4.py
+++ lab4.py
@@ -19,7 +19,31 @@ import numpy as np
 #m = np.array((np.arange(2), np.arange(2)))
 #print(m.shape)
 
-m1 = np.array([[5, 4, 5],[4, 8, 3]])
-print(m1)
-print(m1.shape)
-print(m1.ndim)
+#m1 = np.array([[5, 4, 5],[4, 8, 3]])
+#print(m1)
+#print(m1.shape)
+#print(m1.ndim)
+
+#zera = np.zeros((5, 5))
+#jedynki = np.ones((4,4))
+#print(zera)
+#print(jedynki)
+#print(zera.dtype)
+#print(jedynki.dtype)
+
+#pusta = np.empty((2,2))
+#print(pusta)
+#poz_1 = pusta[1, 1]
+#poz_2 = pusta[0, 1]
+#print(poz_1)
+#print(poz_2)
+
+liczby = np.arange(1, 2, 0.1)
+print(liczby)
+
+liczby_lin = np.linspace(1, 2, 5)
+print(liczby_lin)
+
+z = np.indices((5, 3))
+print(z)
+

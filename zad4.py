11593d8c9af328517d24e0a131b1477ff1cb9d24
jasdsj <jasds2002@gmail.com>
2024-03-27 12:55:32 +0100

    yos

---
+++ zad4.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# zad1
+
+# liczby = np.arange(3, 46, 3)
+# print(liczby)
+
+# zad2
+
+# a = np.array([0.1, 1.1])
+# a = np.arange(2, dtype='int64')
+# print(a.dtype)
+
+# zad3
+
+def zad3(n):
+    m = np.zeros((n,n))
+    yos = 1
+
+    for i in range(n):
+        for j in range(n):
+            m[i, j] = yos
+            yos += 1
+    print(m)
+
+
+def zad4(n, m):
+    x = np.logspace(n, 2,endpoint=False, num=m, base=2 , dtype='int32')
+    print(x)
+
+
+def main():
+
+    # zad3(4)
+    zad4(2, 4)
+
+
+
+
+main()

51c0e413c1e3f0f81afab70d392ef896f5716e75
jasdsj <jasds2002@gmail.com>
2024-03-27 11:43:28 +0100

    yos

--- lab4.py
+++ lab4.py
@@ -1,2 +1,25 @@
-import numpy
+import numpy as np
 
+#a = np.array([0, 1])
+#print(a)
+
+#a = np.arange(2)
+#print(a)
+#print(type(a))
+
+#a = np.arange(2, dtype='int32')
+#print(a.dtype)
+#b = a.astype('float')
+#print(b)
+#print(b.dtype)
+#print(b.shape)
+
+#print(a.ndim)
+
+#m = np.array((np.arange(2), np.arange(2)))
+#print(m.shape)
+
+m1 = np.array([[5, 4, 5],[4, 8, 3]])
+print(m1)
+print(m1.shape)
+print(m1.ndim)
